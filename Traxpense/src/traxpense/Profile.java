205f042ff99cab41d080a2127ccd6882721f0ff9
Trevor <Trevor@T80-FASTCAR>
2019-07-30 21:11:44 -0400

    Added password encryption and decryption

--- Traxpense/src/traxpense/Profile.java
+++ Traxpense/src/traxpense/Profile.java
@@ -10,5 +10,50 @@ package traxpense;
  * @author Trevor
  */
 public class Profile {
+    protected String fName;
+    protected String lName;
+    protected String address;
+    protected String age;
+    protected String email;
+    protected String userName;
+    protected String password;
+    
+    //this should be taken out because we dont want empty instances of Profile
+    public Profile(){
+        
+    }
+    
+    //this can be our worst case instance of a Profile
+    public Profile(String fName, String lName, String userName, String password){
+        this.fName = fName;
+        this.lName = lName;
+        this.userName = userName;
+        this.password = encrypt(password);
+    }
+    
+    /*this function takes in the vanilla password field after it is confirmed 
+    as the password the user wants to use, and ecrypts it to a small extent*/
+    private String encrypt(String vanillaPassword){
+        StringBuilder sb = new StringBuilder();
+        String encryptedPassword;
+        for(int i = 0; i < vanillaPassword.length(); i++){
+            char c = vanillaPassword.charAt(i);            
+            sb.append(String.format("%04x", (int) c));
+        }
+        encryptedPassword = sb.toString();
+        return encryptedPassword;
+    }
+    
+    //decrypts password from file so that checking can occur
+    private String decrypt(String encryptedPassword){
+        StringBuilder sb = new StringBuilder();
+        String decryptedPassword;
+        for (int i = 0; i < encryptedPassword.length(); i+=2) {
+            String str = encryptedPassword.substring(i, i+2);
+            sb.append((char)Integer.parseInt(str, 16));
+        }
+        decryptedPassword = sb.toString();
+        return decryptedPassword;
+    }
     
 }

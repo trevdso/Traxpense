83d41925552a7bd739f9afbdfcdc67e2133729c8
Trevor <Trevor@10.48.33.231>
2019-08-07 10:40:51 -0400

    Added code

--- Traxpense/src/traxpense/Controllers/MainController.java
+++ Traxpense/src/traxpense/Controllers/MainController.java
@@ -1,7 +1,10 @@
 package traxpense.Controllers;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Arrays;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,91 +18,227 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import traxpense.objects.User;
 
+
+/**
+* Main Class Controller that handles sign up and input
+*/
 public class MainController {
-	@FXML private TextField txtFirst;
-	@FXML private TextField txtLast;
-	@FXML private TextField txtUserName;
-	@FXML private TextField txtPassword;
-	@FXML private TextField txtPasswordConfirm;
-	@FXML private Button btnSignUp;
-	@FXML private Button btnLogin;
-	@FXML private Button btnLogo;
+    
+    @FXML private TextField txtFirst;
+    @FXML private TextField txtLast;
+    @FXML private TextField txtUsername;
+    @FXML private TextField txtPassword;
+    @FXML private TextField txtPasswordConfirm;
+    @FXML private TextField txtLogin;
+    @FXML private TextField txtLoginPass;
 
-	@FXML private void initialize() { // lambde expressions for each button sending it to their respective method
+    @FXML private Button btnSignUp;
+    @FXML private Button btnLogin;
+
+    /**
+    * Initialize and add buttons to actions allowing for clicks
+    */
+    @FXML private void initialize() { // lambde expressions for each button sending it to their respective method
+
+        btnSignUp.setOnAction(e -> {onSignUpClicked();} );
+        btnLogin.setOnAction(e -> {onLoginClicked();} );
+
+    }
+
+    /**
+    * On sign up clicked the user's input fields are collected and a new file with the User's info is created
+    */
+    private void onSignUpClicked() { 
+        
+        try{
             
-            btnSignUp.setOnAction(e -> {onSignUpClicked();} );
-            btnLogin.setOnAction(e -> {onLoginClicked();} );
-            btnLogo.setOnAction(e -> {onLogoClicked();} );
-	}
+            User user = new User();
+            user.setFName(txtFirst.getText());
+            user.setLName(txtLast.getText());
+            user.setUsername(txtUsername.getText());
+            user.setPassword(txtPassword.getText(),txtPasswordConfirm.getText());
+            updateFile(user);
 
-	private void onSignUpClicked() { 
-            try{
-                User user = new User();
-                user.setFName(txtFirst.getText());
-                user.setLName(txtLast.getText());
-                user.setUsername(txtUserName.getText());
-                user.setPassword(txtPassword.getText(),txtPasswordConfirm.getText());
-                
-           
-            }catch(IllegalArgumentException n){
-                Alert errorID = new Alert(Alert.AlertType.ERROR, n.getMessage(), ButtonType.OK);
-                errorID.showAndWait();
-                return;
-            }
+        }catch(IllegalArgumentException n){
+            
+            Alert errorID = new Alert(Alert.AlertType.ERROR, n.getMessage(), ButtonType.OK);
+            errorID.showAndWait();
+            
+            return;
+            
+        }
+        //Once done close thi window and go to the dashboard window
+        try {
+            
             Stage stage = (Stage)btnSignUp.getScene().getWindow();
             stage.close();
-            try {
-                AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/traxpense/resources/Dashboard.fxml"));
-                Scene scene = new Scene(root,800,500);
-                scene.getStylesheets().add(getClass().getResource("/traxpense/CSS/traxpenseStyle.css").toExternalForm());
-                Stage secondStage = new Stage();
-                secondStage.setTitle("Dashboard");
-                            secondStage.getIcons().add(new Image(("file:TraxpenseLogo.png")));
-                secondStage.setScene(scene);
-                secondStage.initModality(Modality.APPLICATION_MODAL);  // Use this to make the 2nd window modal (must close 2nd window to return to main window)
-                secondStage.showAndWait();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-	}
-	private void onLoginClicked() { // if login is pressed
-		// Open the second window (stage)
-            try{
-                String dirPath = System.getProperty("user.dir") + "\\src\\traxpense\\data\\";
-                File dir = new File(dirPath);
-                File[] directoryListing = dir.listFiles();
-                System.out.println(Arrays.toString(directoryListing));
-                if (directoryListing.length > 0) {
-                    for (File child : directoryListing) {
+            AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/traxpense/resources/Dashboard.fxml"));
+            Scene scene = new Scene(root,800,500);
+            scene.getStylesheets().add(getClass().getResource("/traxpense/CSS/traxpenseStyle.css").toExternalForm());
+            Stage secondStage = new Stage();
+            secondStage.setTitle("Dashboard");
+            String filePath = System.getProperty("user.dir") + "\\src\\traxpense\\traxpenseIcon.png";
+            secondStage.getIcons().add(new Image(new FileInputStream(filePath)));
+            secondStage.setScene(scene);
+            secondStage.initModality(Modality.NONE);  // Use this to make the 2nd window modal (must close 2nd window to return to main window)
+            secondStage.showAndWait();
+            
+        } catch(Exception e) {
+            
+            e.printStackTrace();
+            
+        }
+        
+    }
+
+    /**
+    * When Log in is clicked we search for the user's file using their 
+    * username and match thir password stored
+    * to check for authorization
+    */
+    private void onLoginClicked() { // if login is pressed
+            // Open the second window (stage)
+        try{
+            String dirPath = System.getProperty("user.dir") + "\\src\\traxpense\\data\\";
+            File dir = new File(dirPath);
+            File[] directoryListing = dir.listFiles();
+            boolean check = false;
+            if (directoryListing.length > 0) {
+                
+                for (File child : directoryListing) {
+                    
+                    BufferedReader buffer = new BufferedReader(new FileReader(child)); 
+                    String st;
+
+                    while ((st = buffer.readLine()) != null){
+                        
+                        String[] vals = st.split(":");
+                        String tempUser = vals[0];
+                        
+                        if(tempUser.equalsIgnoreCase(txtLogin.getText())){
+                            
+                            check = true;
+                            String temp = txtLoginPass.getText().trim();
+                            //encryption was involved at one point but the .equals function wouldnt work with the string
+                            //String pass = decrypt(vals[1]).trim();
+                            String pass = vals[1].trim();
+                            
+                            if(temp.equalsIgnoreCase(pass)){
+                                
+
+                                try {
+                                    
+                                    User user = new User(tempUser, pass, vals[2], vals[3]);
+                                    Stage stage = (Stage)btnLogin.getScene().getWindow();
+                                    stage.close();
+                                    AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/traxpense/resources/Dashboard.fxml"));
+                                    Scene scene = new Scene(root,800,500);
+                                    scene.getStylesheets().add(getClass().getResource("/traxpense/CSS/traxpenseStyle.css").toExternalForm());
+                                    Stage secondStage = new Stage();
+                                    secondStage.setTitle("Dashboard");
+                                    String filePath = System.getProperty("user.dir") + "\\src\\traxpense\\traxpenseIcon.png";
+                                    secondStage.getIcons().add(new Image(new FileInputStream(filePath)));
+                                    secondStage.setScene(scene);
+                                    secondStage.initModality(Modality.NONE);  // Use this to make the 2nd window modal (must close 2nd window to return to main window)
+                                    secondStage.showAndWait();
+                                    
+                                } catch(Exception e) {
+                                    
+                                    e.printStackTrace();
+                                    
+                                }
+                                
+                            }else if(txtLoginPass.getText().isEmpty()){
+                                
+                                Alert errorID = new Alert(Alert.AlertType.ERROR, "Password field is empty", ButtonType.OK);
+                                errorID.showAndWait();
+                                
+                                return;
+                                
+                            }else {
+                                
+                                Alert errorID = new Alert(Alert.AlertType.ERROR, "Invalid Password", ButtonType.OK);
+                                errorID.showAndWait();
+                                
+                                return;
+                                
+                            }
+                            
+                            return;
+                            
+                        }else if(txtLogin.getText().isEmpty()){
+                            
+                            Alert errorID = new Alert(Alert.AlertType.ERROR, "Username field is empty", ButtonType.OK);
+                            errorID.showAndWait();
+                            
+                            return;
+                            
+                        }
                         
                     }
-                } else {
-                    Alert errorID = new Alert(Alert.AlertType.ERROR, "There are no users currently signed up on your system\nSign up!", ButtonType.OK);
-                    errorID.showAndWait();
-                    return;
+
                 }
-            }catch(Exception e){
+                
+                if(check == false){
+                    
+                    Alert errorID = new Alert(Alert.AlertType.ERROR, "No such user on system, sign up!", ButtonType.OK);
+                    errorID.showAndWait();
+                    
+                    return;
+                    
+                }
+                
+            } else {
+                
+                Alert errorID = new Alert(Alert.AlertType.ERROR, "There are no users currently signed up on your system\nSign up!", ButtonType.OK);
+                errorID.showAndWait();
+                
                 return;
-            }
-            Stage stage = (Stage)btnLogin.getScene().getWindow();
-            stage.close();
-            try {
-                AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/traxpense/resources/Dashboard.fxml"));
-                Scene scene = new Scene(root,800,500);
-                scene.getStylesheets().add(getClass().getResource("/traxpense/CSS/traxpenseStyle.css").toExternalForm());
-                Stage secondStage = new Stage();
-                secondStage.setTitle("Dashboard");
-                            secondStage.getIcons().add(new Image(("file:TraxpenseLogo.png")));
-                secondStage.setScene(scene);
-                secondStage.initModality(Modality.APPLICATION_MODAL);  // Use this to make the 2nd window modal (must close 2nd window to return to main window)
-                secondStage.showAndWait();
-            } catch(Exception e) {
-                e.printStackTrace();
+                
             }
             
-	}
+        }catch(Exception e){
+            
+            return;
+            
+        }
         
-	private void onLogoClicked() {
+    }
 
-	}
+    /**
+    * This function writes to file updating User info
+    */
+    private void updateFile(User user) {
+        
+        try{    
+            
+            String absoluteFilePath = System.getProperty("user.dir") + "\\src\\traxpense\\data\\" + user.getFName() + ".txt";
+            File file = new File(absoluteFilePath);
+            
+            if(file.createNewFile()){
+                
+                FileWriter f = new FileWriter(absoluteFilePath, true);
+                String line = String.format("%s:%s:%s:%s\n", user.getUsername(), user.getPassword(), user.getFName(), user.getLName());
+                f.append(line);
+
+                f.close();
+                
+            }else {
+                
+                FileWriter f = new FileWriter(absoluteFilePath, true);
+                String line = String.format("%s:%s:%s:%s\n", user.getUsername(), user.getPassword(), user.getFName(), user.getLName());
+                f.append(line);    
+                f.close();    
+                
+            }
+
+
+        }catch(Exception e){
+            
+            System.out.println(e.getMessage());
+            
+        }
+        
+    }
+    
 }
